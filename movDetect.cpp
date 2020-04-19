58ef55e43c4812dc0661fa9e22ed8b53872e3992
tomaz-suller <tomaz.suller@usp.br>
2020-04-19 17:17:06 -0300

    Add processing function and boundingRect

--- movDetect.cpp
+++ movDetect.cpp
@@ -7,15 +7,27 @@
 
 using namespace cv;
 
+Mat processing(Mat frame){
+
+    cvtColor(frame, frame, CV_RGB2GRAY);
+
+    int dilation_size = 4;
+        
+    threshold(frame, frame, 50, 255, 0);
+    dilate(frame, frame, getStructuringElement(MORPH_RECT, Size(1.5*dilation_size, 1.5*dilation_size), Point(dilation_size, dilation_size)) );
+
+    return frame;
+
+}
+
 int main(){
 
-    Mat frame1;
-    Mat frame2;
-    Mat result, greyresult;
+    Mat frame1, fgm1;
+    Mat frame2, fgm2;
+    Mat result;
 
     int movement;
-    int tresh = 50;
-
+    int tresh = 250;
     VideoCapture vid(0);
 
     if(!vid.isOpened()){ 
@@ -23,29 +35,33 @@ int main(){
         return 1;
     }
 
+    vid >> frame1;
+
     while(true){
 
-        vid >> frame1;
         vid >> frame2;
 
-        result = frame2 - frame1;
+        result = processing(frame2-frame1);
+        Rect movarea = boundingRect(result);
 
-        cvtColor(result, greyresult, CV_RGB2GRAY);
-
-        imshow("Direct Input", frame1);
-        imshow("Direct Subtraction", result);
-        imshow("Grey Subtraction", greyresult);
-
-        for(int i=0; i<greyresult.rows; i++)
-            for(int j=0; j<greyresult.cols; j++)
-                if(greyresult.at<uchar>(i,j) > 50) movement++; 
+        for(int i = movarea.y; i < movarea.height + movarea.y; i++)
+            for(int j = movarea.x; j < movarea.width + movarea.x; j++)
+                if(result.at<uchar>(i,j) > 0) movement++;
         
-        if(movement > tresh) std::cout << "MOVING\n";
-        else std::cout << std::endl;
+        if(movement > tresh){
+            std::cout << "MOVING\n";
+            rectangle(frame1, movarea, Scalar(255, 0, 0), 2);
+        }else std::cout << std::endl;
 
+        imshow("Original", frame1);
+        
+        rectangle(result, movarea, Scalar(255, 0, 0));
+        imshow("Processing", result);
+
+        vid >> frame1;
         movement = 0;
 
-        if(waitKey(100) == 27) break;
+        if(waitKey(10) == 27) break;
 
     }
 
